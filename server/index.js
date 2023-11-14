b6222e14ee933cdf58a97b639014232344c3d474
deepak1210 <deepakkumar.v1210@gmail.com>
2023-11-14 16:57:58 -0600

    Web Socket addition

--- server/index.js
+++ server/index.js
@@ -29,3 +29,42 @@ app.use('',authRouter);
 const server = app.listen(PORT, () => {
     console.log(`listening on ${PORT}...${typeof process.env.URI}`);
 });
+
+const wss = new WebSocketServer({server});
+wss.on("connection", (connection, req)=>{
+    const urlParams = new URLSearchParams(req.url.split("?")[1]);
+    const token = urlParams.get('token');
+    jwt.verify(token,secret,{},(err, data)=>{
+        if (err){
+            console.log(err);
+        }
+        connection.username = data.username;
+        connection.userId = data.userId;
+    });
+    for (const client of [...wss.clients]) {
+        client.send(JSON.stringify({
+            online: [...wss.clients].map(c => c.username),
+        }))
+    }
+    connection.on("message", async (message)=>{
+        const parsedMessage = JSON.parse(message);
+        const {sender, receiver, text} = parsedMessage.message;
+        if(receiver && text){
+            const receiverId = await Users.findOne({username: receiver});
+            const senderId = await Users.findOne({username: sender});
+            const msgDoc = await Chats.create({
+                message: text,
+                senderId: senderId,
+                receiverId: receiverId
+            });
+
+            [...wss.clients]
+                .filter(c => c.username === receiver)
+                .forEach(c => c.send(JSON.stringify({
+                    text,
+                    sender: c.username,
+                    messageId: msgDoc._id
+                })));
+        }
+    });
+});
